24a16ca2126bfbeb97e7ff513c895ad3cf29984b
Josh Holly <joshholly@gmail.com>
2024-09-16 06:43:00 -0500

    Updated EE3

--- src/providers/sources/ee3/index.ts
+++ src/providers/sources/ee3/index.ts
@@ -1,97 +1,36 @@
 import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
-import { Caption } from '@/providers/captions';
-import { compareMedia } from '@/utils/compare';
-import { MovieScrapeContext } from '@/utils/context';
-import { makeCookieHeader } from '@/utils/cookie';
+import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
-import { baseUrl, password, username } from './common';
-import { itemDetails, renewResponse } from './types';
-import { login, parseSearch } from './utils';
+async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
+  let progress = 0;
+  const interval = setInterval(() => {
+    progress += 1;
+    ctx.progress(progress);
+  }, 100);
 
-// this source only has movies
-async function comboScraper(ctx: MovieScrapeContext): Promise<SourcererOutput> {
-  const pass = await login(username, password, ctx);
-  if (!pass) throw new Error('Login failed');
+  const urlEncodedTitle = encodeURIComponent(ctx.media.title);
+  const urlEncodedReleaseYear = encodeURIComponent(ctx.media.releaseYear);
+  const url = `https://ee3.wafflehacker.io/search?id=${urlEncodedTitle}&releaseYear=${urlEncodedReleaseYear}`;
+  const response = await ctx.fetcher(url);
+  ctx.progress(100);
 
-  const search = parseSearch(
-    await ctx.proxiedFetcher<string>('/get', {
-      baseUrl,
-      method: 'POST',
-      body: new URLSearchParams({ query: ctx.media.title, action: 'search' }),
-      headers: {
-        cookie: makeCookieHeader({ PHPSESSID: pass }),
-      },
-    }),
-  );
-
-  const id = search.find((v) => v && compareMedia(ctx.media, v.title, v.year))?.id;
-  if (!id) throw new NotFoundError('No watchable item found');
-
-  const details: itemDetails = JSON.parse(
-    await ctx.proxiedFetcher<string>('/get', {
-      baseUrl,
-      method: 'POST',
-      body: new URLSearchParams({ id, action: 'get_movie_info' }),
-      headers: {
-        cookie: makeCookieHeader({ PHPSESSID: pass }),
-      },
-    }),
-  );
-  if (!details.message.video) throw new Error('Failed to get the stream');
-
-  const keyParams: renewResponse = JSON.parse(
-    await ctx.proxiedFetcher<string>('/renew', {
-      baseUrl,
-      method: 'POST',
-      headers: {
-        cookie: makeCookieHeader({ PHPSESSID: pass }),
-      },
-    }),
-  );
-  if (!keyParams.k) throw new Error('Failed to get the key');
-
-  const server = details.message.server === '1' ? 'https://vid.ee3.me/vid/' : 'https://vault.rips.cc/video/';
-  const k = keyParams.k;
-  const url = `${server}${details.message.video}?${new URLSearchParams({ k })}`;
-  const captions: Caption[] = [];
-
-  // this how they actually deal with subtitles
-  if (details.message.subs?.toLowerCase() === 'yes' && details.message.imdbID) {
-    captions.push({
-      id: `https://rips.cc/subs/${details.message.imdbID}.vtt`,
-      url: `https://rips.cc/subs/${details.message.imdbID}.vtt`,
-      type: 'vtt',
-      hasCorsRestrictions: false,
-      language: 'en',
-    });
+  if (response.statusCode === 404) {
+    throw new NotFoundError('Movie Not Found');
   }
 
-  return {
-    embeds: [],
-    stream: [
-      {
-        id: 'primary',
-        type: 'file',
-        flags: [flags.CORS_ALLOWED],
-        captions,
-        qualities: {
-          // should be unknown, but all the videos are 720p
-          720: {
-            type: 'mp4',
-            url,
-          },
-        },
-      },
-    ],
-  };
+  if (response) return response as SourcererOutput;
+
+  clearInterval(interval);
+  throw new NotFoundError('No data found for this movie');
 }
 
-export const ee3Scraper = makeSourcerer({
+export const filmxyScraper = makeSourcerer({
   id: 'ee3',
   name: 'EE3',
   rank: 111,
+  disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
 });
