d0dfa1922ea7a3d3077125d224275707845a0bd4
Josh Holly <joshholly@gmail.com>
2024-09-26 20:22:51 -0500

    Fixed filelions

--- src/providers/embeds/filelions.ts
+++ src/providers/embeds/filelions.ts
@@ -1,29 +1,34 @@
+import * as unpacker from 'unpacker';
+
 import { flags } from '@/entrypoint/utils/targets';
 import { makeEmbed } from '@/providers/base';
 
-const linkRegex = /file: ?"(http.*?)"/;
-// the white space charecters may seem useless, but without them it breaks
+const packedRegex = /(eval\(function\(p,a,c,k,e,d\).*\)\)\))/;
+const linkRegex = /src:"(https:\/\/[^"]+)"/;
+
 export const filelionsScraper = makeEmbed({
   id: 'filelions',
-  name: 'filelions',
-  rank: 115,
+  name: 'Filelions',
+  rank: 111,
   async scrape(ctx) {
-    const mainPageRes = await ctx.proxiedFetcher.full<string>(ctx.url, {
-      headers: {
-        referer: ctx.url,
-      },
-    });
-    const mainPage = mainPageRes.body;
-    const streamUrl = mainPage.match(linkRegex) ?? [];
-    const playlist = `https://m3u8.wafflehacker.io/m3u8-proxy?url=${encodeURIComponent(streamUrl[1])}`;
-    if (!playlist) throw new Error('Stream url not found');
+    const streamRes = await ctx.proxiedFetcher<string>(ctx.url);
+    const packed = streamRes.match(packedRegex);
 
+    if (!packed) throw new Error('filelions packed not found');
+
+    const unpacked = unpacker.unpack(packed[1]);
+    const link = unpacked.match(linkRegex);
+    const proxiedPlaylist = link
+      ? `https://m3u8.wafflehacker.io/m3u8-proxy?url=${encodeURIComponent(`${link[1]}`)}`
+      : '';
+
+    if (!proxiedPlaylist || proxiedPlaylist === '') throw new Error('filelions file not found');
     return {
       stream: [
         {
-          id: 'primary',
           type: 'hls',
-          playlist,
+          id: 'primary',
+          playlist: proxiedPlaylist,
           flags: [flags.CORS_ALLOWED],
           captions: [],
         },
