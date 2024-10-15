626ae5908fd9bfc786cf19aa0fd07deb1633d380
Josh Holly <joshholly@gmail.com>
2024-10-15 05:29:00 -0500

    Added moviesapi.club

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -61,6 +61,7 @@ import { ee3Scraper } from './sources/ee3';
 import { goojaraScraper } from './sources/goojara';
 import { hdRezkaScraper } from './sources/hdrezka';
 import { m4uScraper } from './sources/m4ufree';
+import { moviesApiScraper } from './sources/moviesapi';
 import { moviplusScraper } from './sources/moviplus';
 import { nepuScraper } from './sources/nepu';
 import { nitesScraper } from './sources/nites';
@@ -104,6 +105,7 @@ export function gatherAllSources(): Array<Sourcerer> {
     moviplusScraper,
     filmxyScraper,
     twoEmbedScraper,
+    moviesApiScraper,
   ];
 }
 
