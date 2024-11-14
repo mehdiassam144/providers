b669cc81c0b5ce5f4604a5951f3c5cd800667b4d
Josh Holly <joshholly@gmail.com>
2024-11-14 01:32:48 -0600

    Fixed soaperTV

--- src/providers/sources/soapertv/index.ts
+++ src/providers/sources/soapertv/index.ts
@@ -8,7 +8,7 @@ import { NotFoundError } from '@/utils/errors';
 import { InfoResponse } from './types';
 import { SourcererOutput, makeSourcerer } from '../../base';
 
-const baseUrl = 'https://soaper.tv';
+const baseUrl = 'https://soaper.live';
 
 const universalScraper = async (ctx: MovieScrapeContext | ShowScrapeContext): Promise<SourcererOutput> => {
   const searchResult = await ctx.proxiedFetcher('/search.html', {
